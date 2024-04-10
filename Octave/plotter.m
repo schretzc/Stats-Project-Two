bf4c8e8653e60e7677c40b0f6fb816ef848e6bed
Christopher Schretzmann <cschretz@gmail.com>
2024-04-10 00:14:58 -0400

    salter

--- Octave/plotter.m
+++ Octave/plotter.m
@@ -1,6 +1,38 @@
-function plotter
-x = linspace(-1000,1000);
-y = x.^2 + 13;
-plot(x,y);
+%references in the code
+%https://stackoverflow.com/questions/25904643/plotting-function-in-octave
+%https://www.youtube.com/playlist?list=PL1A2CSdiySGJ6oZe6XB-TTCFuHc5Fs1PO
+%used those to get familiar with the syntax. used the whole youtube playlist
+%https://docs.octave.org/latest/Defining-Functions.html
+%used to figure out common function syntaxes
+%https://docs.octave.org/v6.4.0/Random-Number-Generation.html
+%to figure out random numbers for salting
+%https://docs.octave.org/latest/The-for-Statement.html
+%for for loops
+%https://docs.octave.org/latest/Cell-Arrays.html
+%for array work
+lowerBound = -100
+upperBound = 100
+x = linspace(lowerBound,upperBound);
+
+function y = plotter(x)
+  y = x.^2 + 13;
 endfunction
-plotter();
+
+yValues = plotter(x);
+%plot(x, yValues);
+
+randomY = yValues
+function y=  salty(y)
+  for i = 1:length(y)
+        y(i) = y(i) + randi([-1000, 1000]);
+  end
+end
+
+randomY = salty(randomY);
+plot(x, randomY);
+
+
+
+
+
+
