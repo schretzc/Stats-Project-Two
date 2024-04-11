31a2376d424d71611d405725a6b3a08300cf34a9
Christopher Schretzmann <cschretz@gmail.com>
2024-04-11 00:48:29 -0400

    external stuff

---
+++ External Library Implementation/JFreePlotter.java
@@ -0,0 +1,9 @@
+//https://www.jfree.org/jfreechart/api/javadoc/index.html
+//https://www.jfree.org/jfreechart/api/javadoc/org/jfree/chart/plot/XYPlot.html
+//https://commons.apache.org/proper/commons-math/commons-math-docs/apidocs/index.html
+
+import org.apache.commons.math4.legacy.stat.descriptive.DescriptiveStatistics;
+public class JFreePlotter {
+    
+    
+}
